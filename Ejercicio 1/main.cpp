5744462d1236586de6a7950ae982401e818f6b8a
Leothomasv <leothomasv@unitec.edu>
2022-07-25 19:50:04 -0600

    cambios

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
+#include <vector>
 
 
 using namespace std;
 
 
-int main() {
+void balance(int cantidad, int core, int cantidadCores) {
+
+	int sumaTotal;
+	int* a = new int(cantidad);
+
+	int pos = cantidad / cantidadCores;
 
 
-
-}
-
-void balance(int cantidad, int core) {
-
+	//lenar el arreglo con los numeros
 	for (int i = 0; i < cantidad; i++)
 	{
+		a[i] = i;
+	}
+
+	while (pos != cantidad) {
+		
+
+
+		
+
+
 
 	}
 
+
+	for (int y = 0; y < cantidad; y++)
+	{
+		cout << " " << a[y];
+	}
+
+}
+
+int main() {
+
+	balance(24, 2, 8);
 }
